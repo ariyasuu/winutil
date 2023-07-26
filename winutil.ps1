59e8fcd349183eb47a18d07bafe6e8c1efb807b1
ariyasu <73967136+ariyasuu@users.noreply.github.com>
2023-07-26 11:43:00 -0300

    Update winutil.ps1

--- winutil.ps1
+++ winutil.ps1
@@ -1153,25 +1153,66 @@ Function Invoke-WPFFormVariables {
 
 
     Write-Host ""
-    Write-Host "    CCCCCCCCCCCCCTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT   "
-    Write-Host " CCC::::::::::::CT:::::::::::::::::::::TT:::::::::::::::::::::T   "
-    Write-Host "CC:::::::::::::::CT:::::::::::::::::::::TT:::::::::::::::::::::T  "
-    Write-Host "C:::::CCCCCCCC::::CT:::::TT:::::::TT:::::TT:::::TT:::::::TT:::::T "
-    Write-Host "C:::::C       CCCCCCTTTTTT  T:::::T  TTTTTTTTTTTT  T:::::T  TTTTTT"
-    Write-Host "C:::::C                     T:::::T                T:::::T        "
-    Write-Host "C:::::C                     T:::::T                T:::::T        "
-    Write-Host "C:::::C                     T:::::T                T:::::T        "
-    Write-Host "C:::::C                     T:::::T                T:::::T        "
-    Write-Host "C:::::C                     T:::::T                T:::::T        "
-    Write-Host "C:::::C                     T:::::T                T:::::T        "
-    Write-Host "C:::::C       CCCCCC        T:::::T                T:::::T        "
-    Write-Host "C:::::CCCCCCCC::::C      TT:::::::TT            TT:::::::TT       "
-    Write-Host "CC:::::::::::::::C       T:::::::::T            T:::::::::T       "
-    Write-Host "CCC::::::::::::C         T:::::::::T            T:::::::::T       "
-    Write-Host "  CCCCCCCCCCCCC          TTTTTTTTTTT            TTTTTTTTTTT       "
+    Write-Host "            *(((     (((((       ((((                                                                       "
+    Write-Host "                ,(((((     ((((((    ((((((                                                                         "
+    Write-Host "               (((((     ((((((*   ((((((                                                                            "
+    Write-Host "               (((((   ((((((((((((((((    ((((((((                                                                 "
+    Write-Host "               ((((((((((((((((((((((((((((((       "
+    Write-Host "               ((((((((((    ((((((((((((((                                                                         "
+    Write-Host "                ((((              (((((((((((((((      "
+    Write-Host "           *(((((((  (((           ((((((((((((     "
+    Write-Host "        ((((((((((             (  /(((( (((((                                                                       "
+    Write-Host "                 ((              (((((    (((((                                                                     "
+    Write-Host "        ((((((((  (               (((       (((((      "
+    Write-Host "        ((((((((  (               (((       (((((       "
+    Write-Host "       (((((((( *(((              (((         ((((("
+    Write-Host "              (((((((            ((((          (((((  "
+    Write-Host "        (((((((((((((           ((((            (((((    "
+    Write-Host "       (((((((((((((((        (((((              (((((      "
+    Write-Host "        ((((((((((((((((((                        ((((                                                              "
+    Write-Host "           (((((((/      (((((                     ####                                                            "
+    Write-Host "                        ((((                    /###                    "
+    Write-Host "                             ((((                    ####                                                           "
+    Write-Host "                              (((/                    ###                                                           "
+    Write-Host "                               ###                    ####                #########################                 "
+    Write-Host "                                ##                     ###           ##################################             "
+    Write-Host "                                *#                     ###       ####################################%%%#%%         "
+    Write-Host "                                 #                      ##     ################################%%%%%%%%%%%%%%%      "
+    Write-Host "               ###########                              ##   ###########################%%%%*          %%%%%%%%%%   "
+    Write-Host "           ####################                          #  ######################%%%%%%%%%%%%%%%%          /%%%%%# "
+    Write-Host "         ##############          #                         ##################%%%%%%%%%%%%%%%%%%%%%%%%%%        %%%%%"
+    Write-Host "        ### ########                                       ###########%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%       %%%"
+    Write-Host "       ### #######       ##                                ####%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%      %%"
+    Write-Host "      ###  #####      ###                                   #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%       %%%%%%      %"
+    Write-Host "       #  #####    ####                                     .%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#      %%%%      "
+    Write-Host "       #  ####    ###                                        *%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%(     %%%      "
+    Write-Host "          ####  ####                      %                    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%     %%%%%%     %%      "
+    Write-Host "         ####  ####                      %%                     %%%%%%%%%%%%%%%%%%%%%%    %%%%    %%%%%      %      "
+    Write-Host "         ###  #####                      %%                      %%%%%%%%%%%%%%%%%%%%       %%     %%%              "
+    Write-Host "         ###  #####.                     %%/                      %%%%%%%%%%%%%%%%%%        %%%                     "
+    Write-Host "        ###   %%%%%%                     %%%                      %%%%%%%%%%%%%%%%%%        ,%#                     "
+    Write-Host "       %%     %%%%%%%                     %%%                    %%%%%%%%%%%%%%%%%%         %                       "
+    Write-Host "     %%%       %%%%%%%                     %%%%               %%%%%%%%%%%%%%%%%%%%/                                 "
+    Write-Host "  %%%           %%%%%%%%                    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%                                  "
+    Write-Host "%%%                 %%%%%%%%%%                   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%                                    "
+    Write-Host "                    %%%%%%%%%%%%%%%%%%%%%        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%                                     "
+    Write-Host "                         %%%%%%%%                   %%%%%%%%%%%%%%%%%%%%%%%%%                                       "
+    Write-Host "                                                           %%%%%%%%%%%%%%                                           "
     Write-Host ""
-    Write-Host "====Chris Titus Tech====="
+    
+    Write-Host "====Vai Tomar No Cu Cristofer====="
     Write-Host "=====Windows Toolbox====="
+                                                                                                                                         
+                                                                
+                                                             
+                                                                
+                                                             
+                                                                 
+                                                              
+                                                           
+                                                        
+                                            
+
 
 
     #====DEBUG GUI Elements====
